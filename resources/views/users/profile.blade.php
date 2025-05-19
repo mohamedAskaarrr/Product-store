e947613d507402febf6d05eaef7c092c3df37c8c
Askarr 230104752 <mohamed230104752@sut.edu.eg>
2025-05-20 00:06:07 +0300

    fav button in userprofile

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -48,12 +48,17 @@
                                 <th>Email</th>
                                 <td>{{ $user->email }}</td>
                             </tr>
+
+
+ 
                             <tr>
                                 <th>Credit Balance</th>
                                 <td>
                                     <span class="badge bg-gold">${{ number_format($user->credit ?? 0, 2) }}</span>
                                 </td>
                             </tr>
+
+                            
                             <tr>
                                 <th>Member Since</th>
                                 <td>{{ $user->created_at ? $user->created_at->format('F j, Y') : 'N/A' }}</td>
@@ -95,6 +100,16 @@
                         <a href="{{ route('edit_password', $user->id) }}" class="btn btn-gold">
                             <i class="fas fa-key"></i> Change Password
                         </a>
+
+                        @role('Customer')
+                            <li class="nav-item">
+                            <a href="{{ route('fav') }}" class="btn btn-gold">
+                            <i  class="fas fa-key"></i> View Favourites
+                            </a>
+                            </li>
+                        @endrole
+                        
+                        
                     </div>
                 </div>
             </div>
