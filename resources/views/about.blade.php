d5529c7800611b33f5931a4dab38a2a8895585d6
Askarr 230104752 <mohamed230104752@sut.edu.eg>
2025-05-19 21:22:52 +0300

    simple about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,5 +6,13 @@
 <div class="container py-4">
     <h1 class="text-gold">About Scentora</h1>
     {{-- Still need to add the content here --}}
+    <h1 class=" btn btn-primary" class="btn btn-primary">Askarrrrrrr </h1>
+    
+    <br>
+    <h1 class=" btn btn-primary" class="btn btn-primary">ZiaAAAAdDD </h1>
+    <br>
+    <h1 class=" btn btn-primary">SAAAAAAAAAAIF </h1>
+    <br>
+    <h1 class=" btn btn-warning">yaaaaaaaaaasssssssssssiiiiiiiiiinnnnnnnnnnnnnnnnnnn </h1>
 </div>
 @endsection
